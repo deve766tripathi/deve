b688042780d70610e539d7af8c650066b7712d1b
deve766tripathi <deveshxiia1384@gmail.com>
2025-07-09 13:21:13 +0530

    changes in pom.xml file

--- deve/src/main/java/com/examle/deve/Service/JwtAuthFilter.java
+++ deve/src/main/java/com/examle/deve/Service/JwtAuthFilter.java
@@ -5,12 +5,13 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.nio.file.attribute.UserPrincipal;
 
-@component
+@Component
 public class JwtAuthFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final CustomUserDetailsService userDetailsService;
